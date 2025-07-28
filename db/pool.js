ef8a37706caaa9d9662bec157ee3e61657e5afde
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2025-07-28 22:27:29 +0700

    Set up pool for db

---
+++ db/pool.js
@@ -0,0 +1,10 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+module.exports = new Pool({
+  host: "localhost",
+  user: process.env.POSTGRES_USER,
+  database: "inventory",
+  password: process.env.POSTGRES_PASS,
+  port: 3000,
+});
