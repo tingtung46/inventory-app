a1cd06ac6599ef97707e5f3b3b248cf1975549cf
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2025-07-31 22:56:30 +0700

    Add query for using db

---
+++ db/query.js
@@ -0,0 +1,49 @@
+const pool = require("./pool");
+
+async function getAllItems() {
+  const { rows } = await pool.query("SELECT * from products");
+  return rows;
+}
+
+async function addItem(item, brand, category, price) {
+  await pool.query(
+    "INSERT INTO products (item, brand, category, price) VALUES ($1, $2, $3, $4)",
+    [item, brand, category, price]
+  );
+}
+
+async function getItem(id) {
+  const { rows } = await pool.query("SELECT * FROM products WHERE id = $1", [
+    id,
+  ]);
+  return rows;
+}
+
+async function updateItem(item, brand, category, price, id) {
+  await pool.query(
+    "UPDATE products SET item = $1, brand = $2, category = $3, price = $4 WHERE id = $5",
+    [item, brand, category, price, id]
+  );
+}
+
+async function searchItem(keyword) {
+  const { rows } = await pool.query(
+    "SELECT * FROM products WHERE item = $1 OR brand = $1 OR category = $1",
+    [`%${keyword}%`]
+  );
+
+  return rows;
+}
+
+async function deleteItem(id) {
+  await pool.query("DELETE FROM products WHERE id = $1", [id]);
+}
+
+module.exports = {
+  getAllItems,
+  addItem,
+  getItem,
+  updateItem,
+  searchItem,
+  deleteItem,
+};
