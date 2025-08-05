053e47d8996f3e69fbf7e199ee04e0b77afcf265
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2025-08-05 22:23:29 +0700

    Add index router

---
+++ routes/indexRouter.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const indexRouter = Router();
+const itemController = require("../controllers/itemController");
+
+indexRouter.get("/", itemController.itemListGet);
+
+indexRouter.get("/new", itemController.addItemGet);
+indexRouter.post("/new", itemController.addItemPost);
+
+indexRouter.get("/update", itemController.updateItemGet);
+indexRouter.post("/update", itemController.updateItemPost);
+
+indexRouter.get("/search", itemController.searchItemGet);
+
+indexRouter.post("/:id/delete", itemController.deleteItem);
+
+module.exports = indexRouter;
