919ac47f078b8178fde9706355fabde27e1c9fd5
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2025-08-20 08:31:01 +0700

    Add main source to run app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("node:path");
+const app = express();
+const indexRouter = require("./routes/indexRouter");
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(assetsPath));
+
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+app.listen(3000);
